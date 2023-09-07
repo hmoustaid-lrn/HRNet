492f9c46d9e2c47eea3b15e840a3f0fc4b1e182d
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-09-07 06:10:30 +0200

    Improve the styling of list employees page

--- src/pages/ListEmployees/index.jsx
+++ src/pages/ListEmployees/index.jsx
@@ -1,12 +1,8 @@
 import { columns } from '../../utils/tools'
 import { useState, useEffect, lazy, Suspense } from 'react'
+import { Link } from 'react-router-dom'
 
 
-/**
- * Function Table retourne
- * un tableau récapitulant les informations des employés
- * @returns component
- */
 function ListEmployees() {
   const DataTable = lazy(() => import('react-data-table-component'))
   const [data, setData] = useState([])
@@ -15,10 +11,7 @@ function ListEmployees() {
     recupereData()
   }, [])
 
-  /**
-   * Function recupereData recupère les données
-   * des employés enregistré dans le localStorage
-   */
+
   function recupereData() {
     const employees = JSON.parse(localStorage.getItem('employees'))
     setData(employees)
@@ -38,30 +31,37 @@ function ListEmployees() {
 
 
 
-  const renderLoader = () => (
-    <div className='loading'>
-      <div id='prim' />
-      <div id='sec' />
-      <div id='cinq' />
-      <div id='quat' />
-    </div>
-  )
+  function loading() {
+    return <h2>🌀 Loading...</h2>
+  }
 
   return (
-    <div className='table'>
-      <Suspense fallback={renderLoader()}>
-        <DataTable
-          columns={columns}
-          data={filteredItems}
-          pagination
-          paginationRowsPerPageOptions={[10, 25, 50, 100]}
-          subHeader
-          persistTableHead
-          noDataComponent='No matching records found'
-          responsive
-        />
-      </Suspense>
-    </div>
+
+    <main className="col-lg-10 col-md-9 mx-auto">
+      <div className="container mb-2 py-2 border rounded">
+        <h2 className="current-employees">Current Employees</h2>
+        <Suspense fallback={loading()}>
+          <DataTable
+            columns={columns}
+            data={filteredItems}
+            pagination
+            paginationRowsPerPageOptions={[10, 25, 50, 100]}
+            subHeader
+            persistTableHead
+            noDataComponent='No matching records found'
+            responsive
+          />
+        </Suspense>
+      </div>
+      <div className="text-center">
+        <Link className="dataTablehomeBtn" to="/">
+          <button type="button" className="btn btn-primary">
+            {' '}
+            Home
+          </button>
+        </Link>
+      </div>
+    </main>
   )
 }
 
