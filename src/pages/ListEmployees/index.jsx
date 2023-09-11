02fdde70d5480dfb5537a38cc15455acbc60eb80
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-09-11 03:00:12 +0200

    Revert "Refactor: Move DataTable to an independent component and add row filtering"
    This reverts commit 57e77efd4b4ed91a9e18f301a299f9a763b42cc2.

--- src/pages/ListEmployees/index.jsx
+++ src/pages/ListEmployees/index.jsx
@@ -1,30 +1,79 @@
-
+import { columns } from '../../utils/tableColumns'
+import { lazy, Suspense } from 'react'
 import { Link } from 'react-router-dom'
 
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+
+import { useState} from "react";
+
+import { deleteEmployeeAction } from "../../state/employees";
 
 
-
-import DataTable from "../../components/DataTable"
 
 
 function ListEmployees() {
-
+  const DataTable = lazy(() => import('react-data-table-component'))
   const employees = useSelector((state) => state.employees.list)
+
+  const dispatch = useDispatch();
+
+
+  const filteredItems = employees?.filter(
+    item => ((item.firstName)) ||
+      (item.lastName) ||
+      (item.department) ||
+      (item.dateOfBirth) ||
+      (item.startDate) ||
+      (item.state) ||
+      (item.city) ||
+      (item.street) ||
+      (item.zipCode)
+  )
+
+
 
   function loading() {
     return <h2>🌀 Loading...</h2>
   }
 
-  return (
+  const [toggleCleared, setToggleCleared] = useState(false);
+
+
+  const handleDeleteRowSelected = ({ selectedRows }) => {
+    if (selectedRows.length !== 0) {
+      const deleteEmployees = window.confirm(`Are you sure you want to delete:\r ${selectedRows.map(r => r.firstName)}?`)
+      if(deleteEmployees){
+        setToggleCleared(!toggleCleared)
+        dispatch(deleteEmployeeAction(selectedRows))
+      }
+    }
     
+  };
+
+
+  return (
+
     <main className="col-lg-10 col-md-9 mx-auto">
       <div className="container mb-2 py-2 border rounded">
         <h2 className="current-employees">Current Employees</h2>
-        <DataTable loading={loading} data={employees} />
+        <Suspense fallback={loading()}>
+          <DataTable
+            columns={columns}
+            data={filteredItems}
+            selectableRows
+            onSelectedRowsChange={handleDeleteRowSelected}
+            clearSelectedRows={toggleCleared}
+            pagination
+            paginationRowsPerPageOptions={[10, 25, 50, 100]}
+            subHeader
+            persistTableHead
+            noDataComponent='No matching records found'
+            responsive
+          />
+        </Suspense>
       </div>
       <div className="text-center">
-        <Link className="home-button" to="/">
+        <Link className="dataTablehomeBtn" to="/">
           <button type="button" className="btn btn-primary">
             {' '}
             Home
