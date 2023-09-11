0a497396107d48a0f4a247297b1495fbfc9962c6
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-09-11 03:22:32 +0200

    Filter table rows

--- src/pages/ListEmployees/index.jsx
+++ src/pages/ListEmployees/index.jsx
@@ -2,13 +2,11 @@ import { columns } from '../../utils/tableColumns'
 import { lazy, Suspense } from 'react'
 import { Link } from 'react-router-dom'
 
-import { useSelector, useDispatch } from "react-redux"
-
-import { useState} from "react";
-
-import { deleteEmployeeAction } from "../../state/employees";
+import { useSelector} from "react-redux"
 
 
+
+import DataTableExtensions from "react-data-table-component-extensions";
 
 
 function ListEmployees() {
@@ -17,24 +15,9 @@ function ListEmployees() {
 
   const employees = useSelector((state) => state.employees.list)
 
-  const dispatch = useDispatch();
 
   function loading() {
     return <h2>🌀 Loading...</h2>
-  }
-
-  const [toggleCleared, setToggleCleared] = useState(false);
-
-
-  const handleDeleteRowSelected = ({ selectedRows }) => {
-    if (selectedRows.length !== 0) {
-      const deleteEmployees = window.confirm(`Are you sure you want to delete:\r ${selectedRows.map(r => r.firstName)}?`)
-      if(deleteEmployees){
-        setToggleCleared(!toggleCleared)
-        dispatch(deleteEmployeeAction(selectedRows))
-      }
-    }
-    
   }
 
 
@@ -44,19 +27,22 @@ function ListEmployees() {
       <div className="container mb-2 py-2 border rounded">
         <h2 className="current-employees">Current Employees</h2>
         <Suspense fallback={loading()}>
-          <DataTable
+        <DataTableExtensions
             columns={columns}
             data={employees}
-            selectableRows
-            onSelectedRowsChange={handleDeleteRowSelected}
-            clearSelectedRows={toggleCleared}
+            print={false}
+            export={false}
+          >
+          <DataTable
             pagination
             paginationRowsPerPageOptions={[10, 25, 50, 100]}
             subHeader
             persistTableHead
+            highlightOnHover
             noDataComponent='No matching records found'
             responsive
           />
+          </ DataTableExtensions>
         </Suspense>
       </div>
       <div className="text-center">
